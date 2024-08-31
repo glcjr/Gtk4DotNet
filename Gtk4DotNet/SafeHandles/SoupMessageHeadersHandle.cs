1ec5fc3c7bc629d3a16be627469adabb2c058891
uriegel <uriegel@hotmail.de>
2024-08-31 15:15:36 +0200

    Bug in SoupMessageHaders unref

--- Gtk4DotNet/SafeHandles/SoupMessageHeadersHandle.cs
+++ Gtk4DotNet/SafeHandles/SoupMessageHeadersHandle.cs
@@ -1,4 +1,4 @@
-using CsTools.Extensions;
+using System.Runtime.InteropServices;
 
 namespace GtkDotNet.SafeHandles;
 
@@ -11,6 +11,9 @@ public class SoupMessageHeadersNewHandle : SoupMessageHeadersHandle
 {
     public SoupMessageHeadersNewHandle() : base() {}
 
-    protected override bool ReleaseHandle() 
-        => true.SideEffect(_ => GObject.Unref(handle));
+    protected override bool ReleaseHandle()
+        => true; // .SideEffect(_ => Unref(handle)); TODO Ref Unref. Use Ref when adding header
+
+    [DllImport(Libs.LibWebKit, EntryPoint = "soup_message_headers_unref", CallingConvention = CallingConvention.Cdecl)]
+    extern static void Unref(nint headers);
 }
